b4abeeb058c675a18e29f30e3bab17f2bb6cb632
Jitendra Patil <jitendracpatil23@gmail.com>
2025-01-14 16:04:30 +0530

    Tasks Management Project

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { Component, NgModule } from '@angular/core';
+ import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add-task', component: AddTaskComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
